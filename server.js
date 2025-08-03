ecda51c924db1e63778b8d493fc23d03dc5710ca
krishnashukla1 <120076833+krishnashukla1@users.noreply.github.com>
2025-08-04 00:55:27 +0530

    test

--- server.js
+++ server.js
@@ -28,5 +28,8 @@ app.post("/api/counter", (req, res) => {
 app.use((req, res, next) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
-
+// ✅ Fallback for SPA (React)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'naamjaap', 'dist', 'index.html'));
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
